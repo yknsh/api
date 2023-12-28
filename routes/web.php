be266b7db575d8278f7d622445970ecc0b4549ab
yknsh <brunopadron623@gmail.com>
2023-12-28 15:09:47 +0200

    front end

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chat',function(){return view('chat');});
+Route::get('/chat/conversation',function(){return view('conversation');});
